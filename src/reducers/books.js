3cde7176551704ecaa3819bd2db7851e1754b77f
Arinpe <dehtolah@gmail.com>
2021-09-20 13:36:48 +0100

    Add reducer folder

---
+++ src/reducers/books.js
@@ -0,0 +1,13 @@
+import { ADD_BOOK, REMOVE_BOOK } from '../actions/type';
+import { defaultBooks } from '../common';
+
+export default (state = defaultBooks, { type, payload }) => {
+  switch (type) {
+    case ADD_BOOK:
+      return [...state, payload];
+    case REMOVE_BOOK:
+      return state.filter(book => book.id !== payload);
+    default:
+      return state;
+  }
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import bookReducer from './books';
+import filterReducer from './filter';
+
+export default combineReducers({
+  books: bookReducer,
+  filter: filterReducer,
+});
