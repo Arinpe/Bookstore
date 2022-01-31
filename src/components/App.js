8c45dd088455ef03b126dd79007518c6c77a482b
Arinpe <dehtolah@gmail.com>
2022-01-31 01:03:17 +0100

    Updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import BooksList from '../containers/BooksList';
 import BookForm from '../containers/BookForm';
-import Form from  '../common/form';
+import Form from  './common/form';
 
 const App = () => (
   <div className="App pb-5">
--- src/components/common/form.js
+++ src/components/common/form.js
@@ -4,24 +4,6 @@ import TextField from '@mui/material/TextField';
 
 export default function BasicTextFields() {
     return (
-        <Box
-            component="form"
-            sx={{
-                '& > :not(style)': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </Box>
-    );
-}
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from './Button';
-export default function BasicTextFields() {
-    return (
         <div>
             <div className="heading-container">
                 <h3>
@@ -40,8 +22,6 @@ export default function BasicTextFields() {
                 <TextField id="email" label="Enter the Email" variant="outlined" />
                 <TextField id="password" label="Enter the Password" variant="outlined" />
             </Box>
-
-            <Button />
         </div>
     );
 }
